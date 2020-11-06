324a0afb0010e26d61bc5e78e80cc0fdb741b28f
Kirk Kornegay <kkornegay1@gmail.com>
2020-11-06 15:43:03 -0500

    last query done

--- query.sql
+++ query.sql
@@ -85,3 +85,8 @@ OR dept_name = 'Development';
 
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
+SELECT last_name, 
+		COUNT(last_name) AS "Frequency Count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency Count" DESC;
